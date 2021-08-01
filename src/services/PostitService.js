85d1b436cc81bc5386da4e4e40b70e48cb793729
Daniel Carvalho <78363490+dan-mcarvalho@users.noreply.github.com>
2021-07-31 22:59:20 -0300

    FINAL COMMIT FRONT

--- src/services/PostitService.js
+++ src/services/PostitService.js
@@ -13,31 +13,39 @@ export default class PostitService{
     static submitForm(postit, userData){
         console.log(postit);
         console.log(userData);
-        const res = axios.post('https://9mcvpqo8wh.execute-api.us-east-2.amazonaws.com/dev/', 
+        axios.post('https://mtkpmqwqf5.execute-api.us-east-1.amazonaws.com/dev/graphql',
+        // axios.post('http://127.0.0.1:3100/graphql', 
         { 
             query: `
             mutation {
-              createTbReserve(
-                  input: { tbReserve: { productName:${postit.name} , productPrice:${postit.price} , productDescription: ${postit.description} , userEmail:${userData} } }
-              ) {
-                  tbReserve {
-                      productName
-                      productPrice
-                      productDescription
-                      userEmail
-                      
-                  }
-              }
-          }`
-        }, 
-        {
+                createTbReserve(
+                    input: { tbReserve: { 
+                      productName:"${postit.title}"  , 
+                      productPrice:"${postit.price}", 
+                      productDescription:"${postit.description}", 
+                      productQuantity:1
+                      userEmail:"${userData.user_email}" } 
+                    }
+                ) {
+                    tbReserve {
+                        productName
+                        productPrice
+                        productDescription
+                        userEmail
+                    }
+                }
+              }`
+        },{
             headers: {
-              'Content-Type': 'text/plain',
-              'Access-Control-Allow-Origin':'*'
-            },
-        })
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+          }
+        }).then(response => {
+            console.log(response.data)
+            alert('Reserva efetuada!')})
+        
 
-        console.log(res.data)
+        
 
         // fetch('https://9mcvpqo8wh.execute-api.us-east-2.amazonaws.com/dev/', 
         // {
@@ -78,38 +86,38 @@ export default class PostitService{
                     {
                         title: "Ocean Blue",
                         img: blue,
-                        price: "5,99",
+                        price: "5.99",
                         description: "It's a postit which looks like the ocean" 
                     },
                     {
                         title: "Pack",
                         img: pack,
-                        price: "5,99",
+                        price: "5.99",
                         description: "It's a useful pack of postits"
                     },
                     {
                         title: "Pinky",
                         img: pink,
-                        price: "1,99",
+                        price: "1.99",
                         description: "It's pinky" 
                     
                     },                   
                     {	
                         title: "Redex",
                         img: red,
-                        price: "1,99",
+                        price: "1.99",
                         description: "IIt's the red one" 
                     },
                     {
                         title: "Super Pink",
                         img: pink2,
-                        price: "5,99",
+                        price: "5.99",
                         description: "It's a pink pack of postits" 
                     },
                     {
                         title: "Purplezoid",
                         img: purple,
-                        price: "1,99",
+                        price: "1.99",
                         description: "It's the purple one" 
                     },
                     ]
